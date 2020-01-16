c4ebcbcf8ce6439df0d2dcb9d19782e84c4c8038
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-16 21:28:42 +0500

    EnglishActivity - XML Done

--- app/src/main/java/fyp/srk/aiduneducateddeafdumb/EnglishAlphabets/EnglishAlphabetsActivity.java
+++ app/src/main/java/fyp/srk/aiduneducateddeafdumb/EnglishAlphabets/EnglishAlphabetsActivity.java
@@ -1,18 +1,15 @@
 package fyp.srk.aiduneducateddeafdumb.EnglishAlphabets;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
-import fyp.srk.aiduneducateddeafdumb.EnglishAlphabets.EnglishAlphabetsFragments.A_character;
 import fyp.srk.aiduneducateddeafdumb.R;
 
 public class EnglishAlphabetsActivity extends AppCompatActivity
 {
+    private ImageView imageReplacer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,19 +17,87 @@ public class EnglishAlphabetsActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_english_alphabets);
 
-
+        imageReplacer = findViewById(R.id.EngAlpha_image_ID);
 
     }
 
-    public void EnglishAlphabets(View view)
+    public void clickA(View view)
     {
-        if (view == findViewById(R.id.character_A_Id))
-        {
-            Fragment fragment = new A_character();
-            FragmentManager fm = getSupportFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
-            ft.replace(R.id.container_Id, fragment);
-            ft.commit();
-        }
+        imageReplacer.setImageResource(R.drawable.a);
+    }
+
+    public void clickB(View view) {
+    }
+
+    public void clickC(View view) {
+    }
+
+    public void clickD(View view) {
+    }
+
+    public void clickE(View view) {
+    }
+
+    public void clickF(View view) {
+    }
+
+    public void clickG(View view) {
+    }
+
+    public void clickH(View view) {
+    }
+
+    public void clickI(View view) {
+    }
+
+    public void clickJ(View view) {
+    }
+
+    public void clickK(View view) {
+    }
+
+    public void clickL(View view) {
+    }
+
+    public void clickM(View view) {
+    }
+
+    public void clickN(View view) {
+    }
+
+    public void clickO(View view) {
+    }
+
+    public void clickP(View view) {
+    }
+
+    public void clickQ(View view) {
+    }
+
+    public void clickR(View view) {
+    }
+
+    public void clickS(View view) {
+    }
+
+    public void clickT(View view) {
+    }
+
+    public void clickU(View view) {
+    }
+
+    public void clickV(View view) {
+    }
+
+    public void clickW(View view) {
+    }
+
+    public void clickX(View view) {
+    }
+
+    public void clickY(View view) {
+    }
+
+    public void clickZ(View view) {
     }
 }

deeb649e7548dc1f688c58dd87543962e215f34d
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-16 19:20:47 +0500

    MainActivity - XML Done

--- app/src/main/java/fyp/srk/aiduneducateddeafdumb/MainActivity.java
+++ app/src/main/java/fyp/srk/aiduneducateddeafdumb/MainActivity.java
@@ -1,14 +1,64 @@
 package fyp.srk.aiduneducateddeafdumb;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button startquiz_Btn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void Launch_EnglishAlphabets(View view)
+    {
+
+    }
+
+    public void Launch_CountingNumbers(View view)
+    {
+
+    }
+
+    public void Launch_FamilyMembers(View view)
+    {
+
+    }
+
+    public void Launch_Greetings(View view)
+    {
+
+    }
+
+    public void Launch_DaysOfTheWeek(View view)
+    {
+
+    }
+
+    public void Launch_TimeActivity(View view)
+    {
+
+    }
+
+    public void Launch_ColorsActivity(View view)
+    {
+
+    }
+
+    public void Launch_EmotionsActivity(View view)
+    {
+
     }
 }

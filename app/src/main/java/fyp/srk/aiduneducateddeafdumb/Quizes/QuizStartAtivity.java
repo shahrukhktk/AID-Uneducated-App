6b342e0538ceeafbdbfc99493396809dc34c30f7
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-18 23:14:42 +0500

    Marks Activities XML Working

---
+++ app/src/main/java/fyp/srk/aiduneducateddeafdumb/Quizes/QuizStartAtivity.java
@@ -0,0 +1,16 @@
+package fyp.srk.aiduneducateddeafdumb.Quizes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fyp.srk.aiduneducateddeafdumb.R;
+
+public class QuizStartAtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_start_ativity);
+    }
+}

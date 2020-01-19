c390e4f6f0fc99daab395a33e1233d9ebd2a7a91
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-19 22:17:54 +0500

    Working on marks sheet xml

---
+++ app/src/main/java/fyp/srk/aiduneducateddeafdumb/MarksSection/MarksSheet.java
@@ -0,0 +1,17 @@
+package fyp.srk.aiduneducateddeafdumb.MarksSection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fyp.srk.aiduneducateddeafdumb.R;
+
+public class MarksSheet extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marks_sheet);
+    }
+}

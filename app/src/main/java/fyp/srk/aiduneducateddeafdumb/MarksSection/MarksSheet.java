ad9031fbeb021a5eeeca7e539cd23ea225a7212a
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-19 22:21:05 +0500

    Working Done for marks sheet xml

--- app/src/main/java/fyp/srk/aiduneducateddeafdumb/MarksSection/MarksSheet.java
+++ app/src/main/java/fyp/srk/aiduneducateddeafdumb/MarksSection/MarksSheet.java
@@ -1,6 +1,8 @@
 package fyp.srk.aiduneducateddeafdumb.MarksSection;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -9,9 +11,18 @@ import fyp.srk.aiduneducateddeafdumb.R;
 public class MarksSheet extends AppCompatActivity
 {
 
+    private RecyclerView recyclerView_Marks;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marks_sheet);
+
+        Toolbar toolbar = findViewById(R.id.marksSectionToolbar_id);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Marks");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerView_Marks = findViewById(R.id.recyclerview_Id);
     }
 }

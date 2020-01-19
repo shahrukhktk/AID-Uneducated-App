8a0bbb5063dabfe4713c3e052dea9d319eecb3f4
shahrukhktk <shahrukhktk760@gmail.com>
2020-01-19 23:00:13 +0500

    Working Done for marks sheet xml

--- app/src/main/java/fyp/srk/aiduneducateddeafdumb/Quizes/QuizActivity.java
+++ app/src/main/java/fyp/srk/aiduneducateddeafdumb/Quizes/QuizActivity.java
@@ -1,6 +1,7 @@
 package fyp.srk.aiduneducateddeafdumb.Quizes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -9,8 +10,14 @@ import fyp.srk.aiduneducateddeafdumb.R;
 public class QuizActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        Toolbar toolbar = findViewById(R.id.quizActivityToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Quiz Activity");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
